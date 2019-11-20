a6be9bfb837ff832d8c7e21be13aa86f14b4c0ec
Vijay Donthireddy <vdonthireddy@gmail.com>
2019-11-19 16:32:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ wlan = WLAN(mode=WLAN.STA)
 
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == 'ServiceChannel-Guest':
+    if net.ssid == '<Your ssid>':
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, 'scfacility81'), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, '<Your wifi password>'), timeout=5000)
         while not wlan.isconnected():
             machine.idle() 
         print('WLAN connection succeeded!')
